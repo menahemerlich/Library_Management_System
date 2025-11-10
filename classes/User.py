52b2a50cc3b0a88059a1e0fd7f7f0019ca730af6
DavidPopovich3261 <8534397@gmail.com>
2025-11-10 15:50:15 +0200

    correction

--- classes/User.py
+++ classes/User.py
@@ -1,11 +1,9 @@
 
-
-
 class User:
-    def __init__(self,name,id ):
-        self.name=name
-        self.id=id
-        self.borrowed_books=[]
+    def __init__(self, name, id ):
+        self.name = name
+        self.id = id
+        self.borrowed_books = []
 
 
 
