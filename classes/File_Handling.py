b69741e90ceeba41251559d33f21fb21af4b5328
DavidPopovich3261 <8534397@gmail.com>
2025-11-11 10:07:24 +0200

    File_Handling

--- classes/File_Handling.py
+++ classes/File_Handling.py
@@ -1,17 +1,23 @@
+from  classes.library import Library,Book, User
 import json
 
+class FileHandling:
+    @staticmethod
+    def guarding_json( library:Library):
+        data={'books':{}, 'users':{}}
+        for book in library.books:
+            data['books'].update(book.__dict__)
+        for user in library.users:
+            data['users'].update(user.__dict__)
+        with open('library.json','w') as j:
+            j.write(json.dumps(data))
 
-class File_Handling:
-    def save_ing(self,l_objects :list):
-        data={}
-        for object_i in l_objects:
-            object=object_i.__dict__
-            data.update(object)
-        json.dumps(data)
-
-
-
-
-    def load_ing(self):
-        pass
-
+    @staticmethod
+    def rescue_from_json(file_name, library:Library = Library([],[])):
+        with open( file_name, "r" ) as j:
+            data = j.read()
+            for book in data['books']:
+                library.add_book(Book(book['title'],book[ 'author'], book['isbn'], book['is_available']))
+            for user in data['users']:
+                library.add_user(User(user['name'],user['id'],user['borrowed_books']))
+            return library
--- classes/User.py
+++ classes/User.py
@@ -1,9 +1,9 @@
 
 class User:
-    def __init__(self, name, id ):
+    def __init__(self, name, id ,borrowed_books:list):
         self.name = name
         self.id = id
-        self.borrowed_books = []
+        self.borrowed_books = borrowed_books
 
 
 
